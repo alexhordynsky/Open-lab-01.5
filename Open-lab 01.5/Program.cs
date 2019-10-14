74bf4796d4be1561c4e8cab51d5bb0dbefb6ee65
z18ista06 <z18ista06@Z00P1-17.SPSKNM>
2019-10-14 09:43:56 +0200

    Add project files.

---
+++ Open-lab 01.5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Open_lab_01._5
+{
+    class Program
+    {
+        public static Boolean Reverse(Boolean sad)
+        {
+            return !sad;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(false));
+        }
+    }
+}
